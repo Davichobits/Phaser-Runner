de7d1058a351ba0b5c3a13a49693e02d56537b72
Davichobits <davidruizdeveloper@gmail.com>
2024-07-25 06:40:41 -0500

    Player jumping

--- src/scenes/main-game.js
+++ src/scenes/main-game.js
@@ -5,6 +5,7 @@ export class MainGame extends Scene {
     constructor() {
         super('Game');
         this.player = null;
+
     }
 
     preload() {
@@ -13,5 +14,19 @@ export class MainGame extends Scene {
 
     create() {
         this.player = new Player(this, 100, 100, 0x000000);
+        // definir teclado
+        this.SPACE = this.input.keyboard.addKey(
+            Phaser.Input.Keyboard.KeyCodes.SPACE
+        );
+    }
+
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(this.SPACE)) {
+            this.jump();
+        }
+    }
+
+    jump() {
+        this.player.body.setVelocityY(-300);
     }
 }
