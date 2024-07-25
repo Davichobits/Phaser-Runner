dc37db2c553432d603aaff9cfca31a11c6479077
Davichobits <davidruizdeveloper@gmail.com>
2024-07-25 06:44:23 -0500

    Sound while jumping

--- src/scenes/main-game.js
+++ src/scenes/main-game.js
@@ -9,7 +9,10 @@ export class MainGame extends Scene {
     }
 
     preload() {
-
+        this.load.audio("coin", "assets/sounds/coin.mp3");
+        this.load.audio("jump", "assets/sounds/jump.mp3");
+        this.load.audio("dead", "assets/sounds/dead.mp3");
+        this.load.audio("theme", "assets/sounds/theme.mp3");
     }
 
     create() {
@@ -18,15 +21,28 @@ export class MainGame extends Scene {
         this.SPACE = this.input.keyboard.addKey(
             Phaser.Input.Keyboard.KeyCodes.SPACE
         );
+        // definir sonido
+        this.loadAudios();
     }
 
     update() {
         if (Phaser.Input.Keyboard.JustDown(this.SPACE)) {
             this.jump();
+            this.playAudio("jump");
         }
     }
 
     jump() {
         this.player.body.setVelocityY(-300);
+    }
+    loadAudios() {
+        this.audios = {
+            jump: this.sound.add("jump"),
+            coin: this.sound.add("coin"),
+            dead: this.sound.add("dead"),
+        };
+    }
+    playAudio(key) {
+        this.audios[key].play();
     }
 }
